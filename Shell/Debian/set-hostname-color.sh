7d081db9fe081f607d18822a7441a2c2d0819ae3
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2025-06-03 16:56:22 +0800

    更新 set-hostname-color.sh

--- Shell/Debian/set-hostname-color.sh
+++ Shell/Debian/set-hostname-color.sh
@@ -1,49 +1,56 @@
 #!/bin/bash
 
-# 颜色映射（数字 => ANSI颜色码）
-declare -A colors=(
-  [1]=31   # 红色
-  [2]=32   # 绿色
-  [3]=33   # 黄色
-  [4]=34   # 蓝色
-  [5]=35   # 紫色
-  [6]=36   # 青色
-  [7]=91   # 亮红色
-  [8]=92   # 亮绿色
-  [9]=93   # 亮黄色
-  [10]=94  # 亮蓝色
-  [11]=95  # 亮紫色
-  [12]=96  # 亮青色
-)
-
-# 打印彩色菜单
 echo "请选择主机名显示颜色："
-echo -e " 1) \e[31m红色\e[0m"
-echo -e " 2) \e[32m绿色\e[0m"
-echo -e " 3) \e[33m黄色\e[0m"
-echo -e " 4) \e[34m蓝色\e[0m"
-echo -e " 5) \e[35m紫色\e[0m"
-echo -e " 6) \e[36m青色\e[0m"
-echo -e " 7) \e[91m亮红色\e[0m"
-echo -e " 8) \e[92m亮绿色\e[0m"
-echo -e " 9) \e[93m亮黄色\e[0m"
-echo -e "10) \e[94m亮蓝色\e[0m"
-echo -e "11) \e[95m亮紫色\e[0m"
-echo -e "12) \e[96m亮青色\e[0m"
+echo -e "1) \e[31m红色\e[0m"
+echo -e "2) \e[32m绿色\e[0m"
+echo -e "3) \e[33m黄色\e[0m"
+echo -e "4) \e[34m蓝色\e[0m"
+echo -e "5) \e[35m紫色\e[0m"
+echo -e "6) \e[36m青色\e[0m"
+echo -e "7) \e[91m亮红\e[0m"
+echo -e "8) \e[92m亮绿\e[0m"
+echo -e "9) \e[93m亮黄\e[0m"
+echo -e "10) \e[94m亮蓝\e[0m"
+echo -e "11) \e[95m亮紫\e[0m"
+echo -e "12) \e[96m亮青\e[0m"
 
-read -rp "输入数字选择颜色 (1-12): " choice
+read -p "输入数字选择颜色 (1-12): " color_choice
 
-if [[ ! ${colors[$choice]+_} ]]; then
-  echo "无效选择，退出"
-  exit 1
+case $color_choice in
+  1) color_code="31" ;;
+  2) color_code="32" ;;
+  3) color_code="33" ;;
+  4) color_code="34" ;;
+  5) color_code="35" ;;
+  6) color_code="36" ;;
+  7) color_code="91" ;;
+  8) color_code="92" ;;
+  9) color_code="93" ;;
+ 10) color_code="94" ;;
+ 11) color_code="95" ;;
+ 12) color_code="96" ;;
+  *) echo "无效选择，退出"; exit 1 ;;
+esac
+
+read -p "是否加粗主机名？(y/n): " bold_choice
+
+if [[ "$bold_choice" =~ ^[Yy]$ ]]; then
+  color_seq="\\[\\e[1;${color_code}m\\]"
+else
+  color_seq="\\[\\e[0;${color_code}m\\]"
 fi
 
-color_code=${colors[$choice]}
+reset_seq="\\[\\e[0m\\]"
 
-# PS1，只给主机名加颜色
-new_ps1='\\u@\\[\e['"$color_code"'m\\]\\h\\[\e[0m\\]:\\w\\$ '
+# 构造 PS1
+new_ps1='\\u@'"${color_seq}"'\\h'"${reset_seq}"':\\w\\$ '
 
-# 写入 /etc/profile.d/hostname_color.sh
-echo "export PS1=\"$new_ps1\"" > /etc/profile.d/hostname_color.sh
+# 修改 .bashrc
+if grep -q '^PS1=' ~/.bashrc; then
+  sed -i 's/^PS1=.*/PS1='"\"$new_ps1\""'/g' ~/.bashrc
+else
+  echo "PS1=\"$new_ps1\"" >> ~/.bashrc
+fi
 
-echo "设置完成，重新登录后主机名将显示为选中的颜色（仅主机名）"
+echo -e "\n已更新主机名颜色为 \e[${color_code}m$(hostname)\e[0m"
+echo "变更将在下次登录或执行 'source ~/.bashrc' 后生效。"
